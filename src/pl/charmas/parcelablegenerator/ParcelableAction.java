9ed47117d88474fc823f6d2f0133a51b4dffb7f2
Michal Charmas <michal@charmas.pl>
2013-04-19 19:22:27 +0200

    Added licence headers.

--- src/pl/charmas/parcelablegenerator/ParcelableAction.java
+++ src/pl/charmas/parcelablegenerator/ParcelableAction.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2013 Michał Charmas (http://blog.charmas.pl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package pl.charmas.parcelablegenerator;
 
 import com.intellij.openapi.actionSystem.AnAction;
@@ -10,7 +25,6 @@ import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.impl.JavaFactoryProvider;
 import com.intellij.psi.util.PsiTreeUtil;
 
 import java.util.List;
@@ -51,7 +65,6 @@ public class ParcelableAction extends AnAction {
         }
         int offset = editor.getCaretModel().getOffset();
         PsiElement element = psiFile.findElementAt(offset);
-        PsiClass psiClass = PsiTreeUtil.getParentOfType(element, PsiClass.class);
-        return psiClass;
+        return PsiTreeUtil.getParentOfType(element, PsiClass.class);
     }
 }
