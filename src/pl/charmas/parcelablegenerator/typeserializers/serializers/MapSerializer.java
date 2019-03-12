739114a92172eee00adb64e6e7cfa9a68b898fa8
Caij <271945881@qq.com>
2019-03-12 13:45:42 +0800

    add map null check

--- src/pl/charmas/parcelablegenerator/typeserializers/serializers/MapSerializer.java
+++ src/pl/charmas/parcelablegenerator/typeserializers/serializers/MapSerializer.java
@@ -25,6 +25,9 @@ import java.util.List;
 
 @SuppressWarnings("StringBufferReplaceableByString")
 public class MapSerializer implements TypeSerializer {
+
+    private static final String NULL_VALUE = "-1";
+
     private final TypeSerializerFactory typeSerializerFactory;
 
     public MapSerializer(TypeSerializerFactory typeSerializerFactory) {
@@ -40,8 +43,10 @@ public class MapSerializer implements TypeSerializer {
 
         final String fieldName = field.getName();
 
+
         return new StringBuilder()
-                .append(parcel).append(".writeInt(").append(fieldName).append(".size());")
+                .append(String.format("%s.writeInt(%s != null ? %s.size() : %s);", parcel, fieldName, fieldName, NULL_VALUE))
+                .append(String.format("if(%s != null) {", fieldName))
                 .append(String.format("for(Map.Entry<%s,%s> entry : %s.entrySet()) {", keyType.getCanonicalText(), valueType.getCanonicalText(), field.getName()))
                 .append(typeSerializerFactory
                         .getSerializer(keyType)
@@ -51,6 +56,7 @@ public class MapSerializer implements TypeSerializer {
                         .getSerializer(valueType)
                         .writeValue(SerializableValue.statement("entry.getValue()", valueType), parcel, flags)
                 )
+                .append("}")
                 .append("}")
                 .toString();
     }
@@ -64,11 +70,13 @@ public class MapSerializer implements TypeSerializer {
         String sizeVariableName = field.getSimpleName() + "Size";
         return new StringBuilder()
                 .append(String.format("int %s = %s.readInt();", sizeVariableName, parcel))
+                .append(String.format("if(%s != %s) {", sizeVariableName, NULL_VALUE))
                 .append(field.getName()).append(String.format(" = new java.util.HashMap<%s, %s>(%s);", keyType.getCanonicalText(), valueType.getCanonicalText(), sizeVariableName))
                 .append(String.format("for(int i = 0; i < %s; i++) {", sizeVariableName))
                 .append(typeSerializerFactory.getSerializer(keyType).readValue(SerializableValue.variable("key", keyType), parcel))
                 .append(typeSerializerFactory.getSerializer(valueType).readValue(SerializableValue.variable("value", valueType), parcel))
                 .append(field.getName()).append(".put(key, value);")
+                .append("}")
                 .append("}")
                 .toString();
     }
